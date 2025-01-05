5477ec32aaf903773c85d738aaa5a9915c57111b
Thomas Bogg <thomas@algarvebeachapartments.com>
2025-01-05 18:26:40 +0000

    Changed logo size

--- navbar.php
+++ navbar.php
@@ -1,20 +1,41 @@
 <nav class="navbar navbar-expand-md">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">
-      <img src="ABA_Logo_FAW.png" width='300' alt="Algarve Beach Apartments">
+
+  <div class="container-fluid sticky-top">
+  
+    <a class="navbar-brand text-center" href="#">
+      <img src="website-logo.png" class='img-fluid' alt="Algarve Beach Apartments">
     </a>
+   
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse fs-5" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto my-2 my-md-0 text-center">
+  
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    
+      <ul class="navbar-nav my-2 my-md-0">
 
         <?php
 
+          $nav_items = array(
+
+              'Home' => '#',
+              
+              'Quinta da Barracuda' => '#',
+              
+              'Clube do Monaco' => '#',
+              
+              'Parque da Corcovada' => '#',
+              
+              'Conditions' => '#',
+
+          );
+
           foreach ($nav_items as $nav_item_name => $nav_item_link) {
           
-            echo "<li class='$navbar_item_class'>
-                  <a class='nav-link' href='$nav_item_link'>$nav_item_name</a>
+            echo "<li class='nav-item ps-md-2 ps-lg-3 ps-xl-4 text-nowrap text-center'>
+             
+                    <a class='nav-link' href='$nav_item_link'>$nav_item_name</a>
+                  
                   </li>";
           
           }
--- variables.php
+++ variables.php
@@ -4,20 +4,4 @@
 
     $my_name = 'Thomas Boggiano';
 
-    $navbar_item_class = 'nav-item px-2 px-lg-4 px-xl-5 text-md-nowrap';
-
-    $nav_items = array(
-
-        'Home' => '#',
-        
-        'Quinta da Barracuda' => '#',
-        
-        'Clube do Monaco' => '#',
-        
-        'Parque da Corcovada' => '#',
-
-    );
-
-    $test = 'Test push from bash ';
-
 ?>
