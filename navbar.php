d0b2b02c565443c8229b4ff85a3d1e8eab2d3edf
Thomas Bogg <thomas@algarvebeachapartments.com>
2025-01-05 19:26:15 +0000

    Added circle logo for responsiveness

--- navbar.php
+++ navbar.php
@@ -1,9 +1,14 @@
 <nav class="navbar navbar-expand-md">
 
-  <div class="container-fluid sticky-top">
+  <div class="container sticky-top">
   
-    <a class="navbar-brand text-center" href="#">
-      <img src="website-logo.png" class='img-fluid' alt="Algarve Beach Apartments">
+    <a class="navbar-brand px-md-2 px-lg-3 px-xl-4 my-0 d-md-none d-xl-block" href="#">
+      <img src="website-logo-full.png" class='img-fluid' alt="Algarve Beach Apartments">
+    </a>
+   
+    
+    <a class="navbar-brand px-md-1 px-lg-3 px-xl-4 my-0 d-none d-lg-block d-xl-none" href="#">
+      <img src="website-logo-circle.png" class='img-fluid' alt="Algarve Beach Apartments">
     </a>
    
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -26,13 +31,13 @@
               
               'Parque da Corcovada' => '#',
               
-              'Conditions' => '#',
+              'Booking Conditions' => '#',
 
           );
 
           foreach ($nav_items as $nav_item_name => $nav_item_link) {
           
-            echo "<li class='nav-item ps-md-2 ps-lg-3 ps-xl-4 text-nowrap text-center'>
+            echo "<li class='nav-item ps-1 px-lg-3 px-xl-4 text-nowrap text-center'>
              
                     <a class='nav-link' href='$nav_item_link'>$nav_item_name</a>
                   
